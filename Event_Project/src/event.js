83c05498dccc33d9972d537b13f91dc358bb1196
mateus_britto <mateusbritto00@gmail.com>
2024-11-02 10:07:14 +0100

    Test commit

--- Event_Project/src/event.js
+++ Event_Project/src/event.js
@@ -1,5 +1,3 @@
-const alertBtn = document.getElementById("inspire-button");
-
 document.getElementById("inspire-button").onclick = function () {
   alert(`“All we have to decide is what to do with the time that is given us.”
 ― J.R.R. Tolkien, The Fellowship of the Ring`);
